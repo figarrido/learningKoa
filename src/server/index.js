59f7d3d0f95dd8e18327c0aca0556c4173a57bbb
Felipe Garrido Berríos <figarrido@uc.cl>
2017-12-10 22:03:19 -0300

    Add koa router and use it in index

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,10 @@
 const Koa = require('koa')
+const indexRouter = require('./routes/index')
 
 const app = new Koa()
 const PORT = 1337
 
-app.use(async ctx => {
-  ctx.body = {
-    status: 'success',
-    message: 'hello world'
-  }
-})
+app.use(indexRouter.routes())
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
